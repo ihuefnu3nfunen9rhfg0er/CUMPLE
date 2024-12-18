5eca6a3bf66758dbd21cfe0b78c297ab28ecdd37
ihuefnu3nfunen9rhfg0er <166246436+ihuefnu3nfunen9rhfg0er@users.noreply.github.com>
2024-12-18 17:54:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("surprise-btn").addEventListener("click", () => {
+    const message = document.getElementById("surprise-message");
+    message.classList.toggle("hidden");
+  });
+  
